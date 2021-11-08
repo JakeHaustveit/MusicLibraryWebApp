62d3616a5ed1777d0ee8b8e3b723ff9b7a0e1f1a
unknown <jacobhaustveit@gmail.com>
2021-11-08 14:08:30 -0500

    Working on finishing Create song app.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 import React, {Component} from "react";
 import DisplayAllMusic from "./components/DisplayAllSongs/DisplayAllSongs";
+import CreateSong from "./components/CreateSong/CreateSong";
 
 
 
@@ -14,19 +15,18 @@ class App extends Component {
 
   }
 
+
   componentDidMount(){
     this.getAllSongs();
   }
 
   getAllSongs = async () => {
     let response = await axios.get('http://127.0.0.1:8000/music/')
-    console.log('hello')
-    this.setState({
-      
+    this.setState({      
       songs: response.data
     });
-    
   }
+
 
 
   render() {
@@ -34,6 +34,7 @@ class App extends Component {
       <div>
       <h1>Hello World</h1>      
       <DisplayAllMusic songs={this.state.songs} />
+      <CreateSong />
       </div>
     )
   }
---
+++ src/components/CreateSong/CreateSong.jsx
@@ -0,0 +1,59 @@
+
+import React, { Component } from "react";
+import axios from "axios";
+import './CreateSong.css'
+
+
+
+class CreateSong extends Component{
+    state= {
+        title:"",
+        artist:"",
+        album:"",
+        release_date:"",
+        likes:""
+    }
+
+    handleChange = (event) => {
+        
+        console.log("hello")
+        this.setState({
+          [event.target.name]: event.target.name,
+        })
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault();  
+        let newSong = this.state
+        console.log(newSong)
+        
+        
+        
+    }
+
+    render() {
+        return(            
+            <form onSubmit={() => this.handleSubmit()} method="post">
+                
+                <label>Title:</label>
+                <div className=''>
+                    <input  type="text" name="title" onChange={this.handleChange} ></input>
+                    <label>Artist Name:</label>
+                    <input type="text" name="artist" onChange={this.handleChange} ></input>
+                    <label>Album Name:</label>
+                    <input type="text" name="album" onChange={this.handleChange} ></input>
+                    <label>Release Date:</label>
+                    <input type="text" name="release_date" onChange={this.handleChange} ></input>
+                    <label>Likes:</label>
+                    <input type="text" name="likes" onChange={this.handleChange} value='0' ></input>
+                    <button type="submit">Add Song</button>
+                </div>
+                          
+            </form>
+        )
+    }     
+}
+
+
+
+export default CreateSong;
